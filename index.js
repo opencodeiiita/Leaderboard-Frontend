940345808c6fa412aff221061025e21cc7c83d07
batflarrow <dwivedia2001@gmail.com>
2021-10-07 16:50:42 +0530

    Add styling to page

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
+const confettiCanvas = document.querySelector('.tsparticles-canvas-el');
+const header = document.querySelector("header");
+var toggleButton = document.getElementById("toggle");
+$(document).ready(() => {
+    let isDarkMode = localStorage.getItem('dark');
+    if (isDarkMode === null)
+        return localStorage.setItem('dark', 0);
+    if (isDarkMode == 1) {
+        console.log('came here2');
+        toggleButton.classList.add("active");
+        confettiCanvas.style.backgroundColor = "#232323";
+        header.style.color = "white";
+    }
+});
+toggleButton.onclick = function () {
+    toggleButton.classList.toggle("active");
+    let isDarkMode = localStorage.getItem('dark');
+    if (isDarkMode == 0) {
+        confettiCanvas.style.backgroundColor = "#232323";
+        header.style.color = "white";
+        isDarkMode = 1;
+    }
+    else {
+        confettiCanvas.style.backgroundColor = "#E6F4F2";
+        header.style.color = "black";
+        isDarkMode = 0;
+    }
+    localStorage.setItem('dark', isDarkMode);
+};
 var page = 1;
 var rank = 0;
 var lastScore = -1;
 var hasNext = true;
 let isFetching = true;
 const $end = document.querySelector('#end');
-const $logo = document.querySelector('#logo');
+const $loader = document.querySelector('.loader');
 async function getData() {
-    $logo.style.display = "";
+    $loader.classList.remove('hide');
     let res = await fetch(`https://opencodeiiita.herokuapp.com/get-all-data/?page=${page}`);
     let data = await res.json();
-    $logo.style.display = "none";
+    $loader.classList.add('hide');
     return data;
 }
 window.addEventListener('scroll', () => {
@@ -50,56 +79,42 @@ function addToTable(arr) {
             lastScore = points;
             rank++;
         }
+        let rankClass = "";
         switch (rank) {
             case 1:
-                markup =
-                    '<tr style="background-color: gold;"><td>' +
-                    rank +
-                    '&nbsp;' +
-                    '</td><td>' +
-                    name +
-                    '</td><td> ' +
-                    '&nbsp;' +
-                    points +
-                    '</td></tr>';
+                rankClass = " first";
                 break;
             case 2:
-                markup =
-                    '<tr style="background-color: silver;"><td>' +
-                    rank +
-                    '&nbsp;' +
-                    '</td><td>' +
-                    name +
-                    '</td><td> ' +
-                    '&nbsp;' +
-                    points +
-                    '</td></tr>';
+                rankClass = " second";
                 break;
             case 3:
-                markup =
-                    '<tr style="background-color: goldenrod;"><td>' +
-                    rank +
-                    '&nbsp;' +
-                    '</td><td>' +
-                    name +
-                    '</td><td> ' +
-                    '&nbsp;' +
-                    points +
-                    '</td></tr>';
+                rankClass = " third";
                 break;
 
             default:
-                markup =
-                    '<tr><td>' +
-                    rank +
-                    '&nbsp;' +
-                    '</td><td>' +
-                    name +
-                    '</td><td> ' +
-                    '&nbsp;' +
-                    points +
-                    '</td></tr>';
+                rankClass = "";
         }
-        $('table tbody').append(markup);
+
+        const markup = `
+       
+        <li class="user${rankClass} zoom">
+        <div class="rank">
+        ${rank}
+        </div>
+        <div class="profile">
+        <a target="_blank" href="https://github.com/${name}" class="githubLink">
+                <img src="https://avatars.githubusercontent.com/${name}?size=200" alt="profile"> 
+                </a>
+                </div>
+                <div class="username">
+                <a target="_blank" href="https://github.com/${name}" class="githubLink">
+                ${name}
+                </a>
+                </div>
+            <div class="points">
+                ${points} pts.
+            </div>
+        </li>    `;
+        $('.rank-list').append(markup);
     }
 }
